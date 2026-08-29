8f87aedefbfcbd4e86eff82725492b22908e86c6
Vadym Yehorov <user@MacBook-Pro.lan>
2026-08-29 19:04:34 -0400

    drop: remove cover-image upload from project create flow

--- apps/web/core/components/projects/create/utils.ts
+++ apps/web/core/components/projects/create/utils.ts
@@ -4,19 +4,10 @@
  * See the LICENSE file for details.
  */
 
-import { RANDOM_EMOJI_CODES } from "@plane/constants";
 import type { IProject } from "@plane/types";
-import { getRandomCoverImage } from "@/helpers/cover-image.helper";
 
 export const getProjectFormValues = (): Partial<IProject> => ({
-  cover_image_url: getRandomCoverImage(),
   description: "",
-  logo_props: {
-    in_use: "emoji",
-    emoji: {
-      value: RANDOM_EMOJI_CODES[Math.floor(Math.random() * RANDOM_EMOJI_CODES.length)],
-    },
-  },
   identifier: "",
   name: "",
   network: 2,
